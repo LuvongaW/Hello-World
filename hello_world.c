7b4c67a23e727bfac6b6c9f12efc92069aefca24
LuvongaW <168713095+LuvongaW@users.noreply.github.com>
2026-02-21 14:44:25 +0300

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+printf("University Name: KCA Univeristy\n");
+printf("Student Name:Wayne Luvonga\n");
+printf("Admission Number:25/06259\n");
+printf("Email Address:luvongawayne629@gmail.com\n");
+printf("Home county: Busia\n");
+
+return 0;
+
+
+
+}
